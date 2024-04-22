efb45efb49387b5cdbebc591d000ada3dfb13f40
nkhorshid <neilufar.khorshid@gmail.com>
2024-04-22 14:52:26 +0200

    Moving the code file into a folder that matches the python package name.

---
+++ src/SimAb_test/__init__.py
@@ -0,0 +1,8 @@
+'''
+Modified: Oct 23rd
+@author: N. Khorshid
+
+This is the chemistry module.
+This module calculates the dust to gas ratio, and the abundances of different elements at a given temperature.
+'''
+import numpy as np
